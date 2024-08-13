a8dd73103e35937ac5aac14b6d4faac0cdbc1b0c
Micael Abud <84108378+micas-tsx@users.noreply.github.com>
2024-08-13 16:46:52 -0300

    dados-iniciais

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15),category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2021, 9, 15),category: 'food', title: 'Burguer King', value: 28.00 },
+    { date: new Date(2021, 9, 15),category: 'rent', title: 'Aluguel APt', value: 2300 },
+    { date: new Date(2021, 10, 15),category: 'salary', title: 'Salário', value: 4500 },
+]
